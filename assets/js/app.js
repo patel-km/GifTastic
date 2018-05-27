87b4f632b80fe450507232d6c61e45047dcf33ad
patel-km <karipatel95@hotmail.com>
2018-05-26 20:38:46 -0400

    user generated buttons working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,58 @@
+//We will have 3 onclick functions for buttons -- one for existing buttons, one for the submit input, and one for the new buttons
 
-//we will have 3 onclick functions for buttons -- one for existing buttons, one for the submit input, and one for the new buttons
-//we will also have an onclick function to start and stop the gifs displayed. IF the gif is playing, a click stops the animation. If gif is still, a click makes the gif play.
+//We will also have an onclick function to start and stop the gifs displayed. IF the gif is playing, a click stops the animation. If gif is still, a click makes the gif play.
+
 // BUTTONS EVENTS __________________________________________________________________________________________________________________________________________________
+    
+    //onclick event listener for user input
+    //create a new button and print it to the screen
+    $("#submit").on("click", function () {
+        var input = $("#mood-input").val().trim();
+        var newButton = $("<button>").addClass("new");
+
+        newButton.attr("data-item", input);
+        newButton.text(input);
+
+        $("#moodButtons").append(newButton);
+
+        $("<input>").empty();
+
+    //onclick event listener for new buttons
+    //same function as existing buttons, but just calling it by its diff class
+        $(".new").on("click", function () {
+
+            $("#mood-gifs").empty();
+            
+            var mood = $(this).attr("data-item");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + mood + "&api_key=nbFBZtTWn5t69XGfIAyKL0wRNdAlEuBk&limit=10"
+            
+            $.ajax({
+            url: queryURL,
+            method: "GET"
+            })
+            .then(function(response) {
+                
+                var results = response.data;
+                for (var i = 0; i < results.length; i++) {
+    
+                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                
+                        var rating = results[i].rating;
+                        var p = $("<p>").text("Rating: " + rating);
+                        var moodImage = $("<img>");
+                        moodImage.attr("src", results[i].images.fixed_height.url);
+                    
+                    $("#mood-gifs").append(p);
+                    $("#mood-gifs").append(moodImage);
+    
+                    console.log("works")
+                    }
+                }   
+            })
+            });
+    });
+
+    
     //onclick event listener for existing buttons
     $(".existing").on("click", function () {
 
@@ -49,47 +100,9 @@
         });
 
 
-    //onclick event listener for user input
-    //create a button and print it to the screen
-    $("#submit").on("click", function () {
-        var input = $("#mood-input").val().trim();
-        var newButton = $("<button data-item = '" + input + "'>").addClass("new");
-        newButton.text(input);
+   
+    
 
-        // newButton.attr("data-item = " + input);
-
-        $("#moodButtons").append(newButton);
-
-        console.log("where it go");
-    });
-
-
-    //onclick event listener for new buttons
-    //same function as existing buttons, but just calling it by its diff class
-    $(".new").on("click", function () {
-        $("#mood-gifs").empty();
-        var mood = $(this).attr("data-item");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + mood + "&api_key=nbFBZtTWn5t69XGfIAyKL0wRNdAlEuBk&limit=10"
-        
-        $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-
-        .then(function(response) {
-            var results = response.data;
-            for (var i = 0; i < results.length; i++) {
-                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                    var rating = results[i].rating;
-                    var p = $("<p>").text("Rating: " + rating);
-                    var moodImage = $("<img>");
-                    moodImage.attr("src", results[i].images.fixed_height.url);
-                $("#mood-gifs").append(p);
-                $("#mood-gifs").append(moodImage);
-                }
-            }   
-        })
-    });
 
 
 
